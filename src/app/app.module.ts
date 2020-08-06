d8d717bc3dcdd8ce49c930f50f01775d687330aa
mreyes-academy <mreyes@blastasia.com>
2020-08-06 21:14:49 +0800

    - initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
+import { StopTrainingComponent } from './training/current-training/stop-training.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingsComponent,
+    WelcomeComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    StopTrainingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
+  // entryComponents:[StopTrainingComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/training/current-training/current-training.component.ts
@@ -0,0 +1,47 @@
+import { StopTrainingComponent } from './stop-training.component';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-current-training',
+  templateUrl: './current-training.component.html',
+  styleUrls: ['./current-training.component.css']
+})
+export class CurrentTrainingComponent implements OnInit {
+
+  @Output() trainingExit = new EventEmitter<void>();
+
+  progress = 0;
+
+  timer: number;
+
+  constructor(private dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.startOrResume();
+  }
+
+  startOrResume() {
+    this.timer = setInterval(() => {
+      this.progress = this.progress + 5;
+    }, 1000);
+  }
+
+  onStop() {
+    clearInterval(this.timer);
+    const dialogRef = this.dialog.open(StopTrainingComponent, {
+      data: {
+        progress: this.progress
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.trainingExit.emit();
+      } else {
+        this.startOrResume();
+      }
+    });
+  }
+
+}
